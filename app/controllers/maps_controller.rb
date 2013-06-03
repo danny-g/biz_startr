38e69777541fcbf575b55715edcc1955a5b945a3
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-02 21:39:52 -0600

    maps#show

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,10 @@
 class MapsController < ApplicationController
 
   def index
-    gon.reqs = StateRequirement.all
+    # gon.reqs = StateRequirement.all
+  end
+
+  def show
+    @state_reqs = StateRequirement.where state_abbr: params[:id]
   end
 end
