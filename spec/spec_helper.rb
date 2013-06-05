72ec1704322cc7bbd2ce33506522d919fa7162a6
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-05 13:05:13 -0600

    require vcr if Rails.env.test? == true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'vcr' if ENV["RAILS_ENV"] == 'test'
+if Rails.env.test?
+  require 'vcr'
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
